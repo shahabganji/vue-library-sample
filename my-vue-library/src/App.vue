e0a8409033303ae9e5fca068195312b734fb58ae
Saeed Ganji <saeed.ganji@ringana.com>
2021-08-30 13:35:08 +0200

    Initial

---
+++ my-vue-library/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <shahab-banner>
+    This is the sample component banner
+  </shahab-banner>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// @ts-ignore
+import ShahabBanner from './components/Banner.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    'shahab-banner': ShahabBanner
+  }
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-library/tests/unit/example.spec.ts
@@ -0,0 +1,5 @@
+describe('My Library ', () => {
+  it('Checks for sanity of jest', () => {
+    expect(true).toBeTruthy();
+  })
+})
